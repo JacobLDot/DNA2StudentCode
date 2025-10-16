6279b79c4a8eb7cbdfb95b9cdf32eaa72e18d14a
jacobldot <jacob.lowe@menloschool.org>
2025-10-16 13:41:55 -0700

    .

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,9 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date       |     Time     |                                                            Update |
+|:-----------|:------------:|------------------------------------------------------------------:|
+| October 16 | 12:35 - 1:45 | I started coding linear code so I have a place to start off from. |
+|            |              |                                                                   |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/DNA.java
+++ src/DNA.java
@@ -16,7 +16,24 @@ public class DNA {
      * TODO: Complete this function, STRCount(), to return longest consecutive run of STR in sequence.
      */
     public static int STRCount(String sequence, String STR) {
-
-        return 0;
+        int times = 0;
+        int counter = 0;
+        int maxTimes = 0;
+        int keyLength = STR.length();
+        for (int i = 0; i < sequence.length(); i++) {
+            times = 0;
+            while (true) {
+                if (sequence.substring(i + (times * keyLength), i + (times * keyLength) + keyLength) == STR) {
+                    times += 1;
+                    if (times > maxTimes) {
+                        maxTimes = times;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(maxTimes);
+        return maxTimes;
     }
 }
