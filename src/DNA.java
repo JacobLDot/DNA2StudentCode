a192f4c719db651e3f794eb9ecedcd307dd743fe
jacobldot <jacob.lowe@menloschool.org>
2025-10-18 17:11:26 -0700

    Linear STRCount v2

--- src/DNA.java
+++ src/DNA.java
@@ -16,8 +16,13 @@ public class DNA {
      * TODO: Complete this function, STRCount(), to return longest consecutive run of STR in sequence.
      */
     public static int STRCount(String sequence, String STR) {
+
+        // Variables to keep track of STR count and STR length
         int maxTimes = 0;
         int keyLength = STR.length();
+
+        // Go through DNA sequence and check if the sequence
+        // starting with each character is equal to the STR
         for (int i = 0; i <= sequence.length() - keyLength; i++) {
             int times = 0;
             while (sequence.startsWith(STR, i + times * keyLength)) {
@@ -27,6 +32,8 @@ public class DNA {
                 maxTimes = times;
             }
         }
+
+        // Print out and return STR count.
         System.out.println(maxTimes);
         return maxTimes;
     }
