6a346a9f9cba4737e0fb3de6fe23ea33691e67e8
jacobldot <jacob.lowe@menloschool.org>
2025-10-23 15:00:17 -0700

    Hashing v1 of Project v2

--- src/DNA.java
+++ src/DNA.java
@@ -1,3 +1,5 @@
+import static java.util.Objects.hash;
+
 /**
  * DNA
  * <p>
@@ -12,32 +14,32 @@
 
 public class DNA {
 
-    /**
-     * TODO: Complete this function, STRCount(), to return longest consecutive run of STR in sequence.
-     */
     public static int STRCount(String sequence, String STR) {
+        int strCount = 0;
+        int strLength = STR.length();
+        long strHash = hash(STR, strLength);
+        long seqHash = hash(sequence.substring(0, strLength - 1), strLength);
+        for (int i = STR.length(); i < sequence.length() - 1; i++) {
+            if (strHash == seqHash) {
 
-        // Variables to keep track of STR count and STR length
-        int maxTimes = 0;
-        int keyLength = STR.length();
-
-        // Go through DNA sequence and check if the sequence
-        // starting with each character is equal to the STR
-        for (int i = 0; i <= sequence.length() - keyLength; i++) {
-            int times = 0;
-            while (sequence.startsWith(STR, i + times * keyLength)) {
-                times++;
+                // Begin checking for consecutive appearances
+                while (strHash == seqHash) {
+                    strCount++;;
+                    seqHash = hash(sequence.substring(i, i + strLength), strLength);
+                }
             }
-
-            // If there is a longer string of STRs in the
-            // DNA sequence then set the new longest.
-            if (times > maxTimes) {
-                maxTimes = times;
-            }
+            seqHash = hash(sequence.substring(1 + (i - STR.length()), i), strLength);
         }
+        return strCount;
+    }
 
-        // Print out and return STR count.
-        System.out.println(maxTimes);
-        return maxTimes;
+    public static long hash(String STR, int strLength) {
+        int R = 255;
+        long h = 0;
+        long p = 54321102419L;
+        for (int i = 0; i < strLength; i++) {
+            h = (h * R + STR.charAt(i)) % p;
+        }
+        return h;
     }
 }
